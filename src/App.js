650ddefc3d18213f0129672aef0c616c19d6f4d8
Shakhzod <forworkingappsinmylaptop@gmail.com>
2023-06-05 11:45:20 +0500

    create useBookSearch custom in hook in react in order to search books accordingly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useCallback } from "react";
+import TextField from "@mui/material/TextField";
+import useBookSearch from "./useBookSearch";
 
-function App() {
+import "./App.css";
+
+export default function App() {
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const { books, hasMore, loading, error } = useBookSearch(query, pageNumber);
+
+  const observer = useRef();
+  const lastBookElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
+
+  function handleSearch(e) {
+    setQuery(e.target.value);
+    setPageNumber(1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        margin: "2rem",
+        width: "400px",
+      }}
+    >
+      <TextField
+        sx={{ margin: "0 auto" }}
+        id="outlined-basic"
+        label="Search book"
+        variant="outlined"
+        autoFocus
+        value={query}
+        onChange={handleSearch}
+      />
+
+      {books.map((book, index) => {
+        if (books.length === index + 1) {
+          return (
+            <div ref={lastBookElementRef} key={book}>
+              {book}
+            </div>
+          );
+        } else {
+          return <div key={book}>{book}</div>;
+        }
+      })}
+      <h1>{loading && "Loading..."}</h1>
+      <div>{error && "Error"}</div>
     </div>
   );
 }
-
-export default App;
